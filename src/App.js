c7421d813202289141eaae2690506844d8cb4d9b
vksawe <vskiprotich@gmail.com>
2020-03-28 13:50:02 +0300

    prop-type validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
+import Burger from './components/Burger/Burger'
 
 function App() {
   return (
     <div >
       <Layout>
       <BurgerBuilder/>
+      <Burger/>
       </Layout>
     </div>
   );
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
+const Burger=(props)=>{
+return(
+    <BurgerIngredients type="salad"/>
+)
+}
+
+export default Burger;
