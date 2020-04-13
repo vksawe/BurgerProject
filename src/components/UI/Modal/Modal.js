86131fa1b82639518365806c174a29f3e13dd807
vksawe <vskiprotich@gmail.com>
2020-04-13 20:49:35 +0300

    minor  optimization tweaks

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.module.css'
 import Auxillary from '../../../hoc/Auxillary';
 import Backdrop from '../Backdrop/Backdrop'
-const Modal=(props)=>{
+class Modal extends Component{
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show!==this.props.show
+    }
+   
+    render(){
 return (
     <Auxillary>
-        <Backdrop  clicked={props.modalClosed} show={props.show}/>
+        <Backdrop  clicked={this.props.modalClosed} show={this.props.show}/>
     <div  className={classes.Modal} style={{
-    transform:props.show ? 'translateY(0)':'translateY(-100vh)',
-    opacity:props.show? '1':'0'
+    transform:this.props.show ? 'translateY(0)':'translateY(-100vh)',
+    opacity:this.props.show? '1':'0'
 }}>
-    {props.children}
+    {this.props.children}
 </div>
 </Auxillary>
 )
+}
 }
 
 export default Modal;
